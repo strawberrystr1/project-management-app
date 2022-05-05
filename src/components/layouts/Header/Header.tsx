3e79232dbcafcb1b3c6603110a101b9ed7a93f4a
Denis Popov <popovdw94@gmail.com>
2022-05-05 21:52:12 +0300

    feat: add i18next deps and i18n config

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,10 +1,15 @@
 import { Container } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import styles from './style.module.scss';
 
 const Header = () => {
+  const { t } = useTranslation();
   return (
     <Container maxWidth={false} component="header" className={styles.headerWrapper}>
-      <Container maxWidth="xl">header</Container>
+      <Container maxWidth="xl">
+        header
+        <h3>{t('example.header')}</h3>
+      </Container>
     </Container>
   );
 };
