52b7a4952d2710d86179b126889648a7830424b1
strawberrystr1 <andrey_overdose@mail.ru>
2022-05-08 18:25:24 +0300

    feat: add translation

--- src/utils/constants/teamInfo.ts
+++ src/utils/constants/teamInfo.ts
@@ -1,17 +1,14 @@
 const team = [
   {
     name: 'Andrei Shakh',
-    info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, explicabo.',
     github: 'https://github.com/strawberrystr1',
   },
   {
     name: 'Dmytro Zozuliak',
-    info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, explicabo.',
     github: 'https://github.com/DmytroZozuliak',
   },
   {
     name: 'Denis Popov',
-    info: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse, explicabo.',
     github: 'https://github.com/denis0ff',
   },
 ];
