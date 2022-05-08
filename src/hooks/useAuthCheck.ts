ecd4dae2ef654cd9e50917be96da99d3f404eedf
strawberrystr1 <andrey_overdose@mail.ru>
2022-05-08 18:53:29 +0300

    refactor: delete unused variable

--- src/hooks/useAuthCheck.ts
+++ src/hooks/useAuthCheck.ts
@@ -1,15 +1,10 @@
 import { useEffect } from 'react';
-import jwt from 'jwt-decode';
 import { useGetUserMutation } from '../store/services/userService';
 import { useTypedSelector } from './redux';
 
 const useAuthCheck = () => {
   const { token } = useTypedSelector((state) => state.user);
-  let id = '';
-  if (token) {
-    const data: { userId: string } = jwt(token);
-    id = data.userId;
-  }
+
   const [getUser] = useGetUserMutation();
 
   useEffect(() => {
