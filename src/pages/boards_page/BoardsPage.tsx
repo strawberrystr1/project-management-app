f241935f1b640b14f25a1c75da957bd78555b1dd
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-05-09 22:06:47 +0300

    feat: add board btn

--- src/pages/boards_page/BoardsPage.tsx
+++ src/pages/boards_page/BoardsPage.tsx
@@ -12,7 +12,11 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
+import CreateBoardForm from '../../components/CreateBoardForm';
+import DialogButton from '../../components/layouts/DialogButton';
 import styles from './style.module.scss';
 
 const boardsMock = [
@@ -23,7 +27,37 @@ const boardsMock = [
   { id: '5', title: 'board 5' },
 ];
 
+// const boardsMock = [];
+
 const Boards = () => {
+  const createAddBtn = (handleOpen: () => void) => {
+    return (
+      <Grid onClick={handleOpen} item xs={2} sm={4} md={4}>
+        <Card
+          sx={{
+            height: '100%',
+            maxWidth: 345,
+            backgroundColor: 'secondary.main',
+            // backgroundColor: 'transparent',
+            border: '1px solid black',
+          }}
+        >
+          <CardActionArea
+            sx={{
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            onClick={() => console.log('ADD')}
+          >
+            <AddIcon fontSize="large" />
+          </CardActionArea>
+        </Card>
+      </Grid>
+    );
+  };
+
   return (
     <>
       <TextField id="standard-basic" label="Search" variant="standard" />
@@ -35,9 +69,11 @@ const Boards = () => {
       >
         {boardsMock.map((board) => (
           <Grid item xs={2} sm={4} md={4} key={board.id}>
-            {/* {background: "main.primary"} */}
             <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea onClick={() => console.log('open card')}>
+              <CardActionArea
+                // sx={{ backgroundColor: 'secondary.main' }}
+                onClick={() => console.log('open card', board.id)}
+              >
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
                     {board.title}
@@ -48,34 +84,28 @@ const Boards = () => {
                 </CardContent>
               </CardActionArea>
               <Divider />
-              <Button size="small" fullWidth onClick={() => console.log('delete')} color="error">
+              <Button
+                size="small"
+                // variant="contained"
+                // variant="outlined"
+                // variant="text"
+                fullWidth
+                onClick={() => console.log('delete card', board.id)}
+                // color="error"
+                // color="secondary"
+                // color="info"
+                color="warning"
+              >
                 Delete
               </Button>
             </Card>
           </Grid>
         ))}
-        <Grid item xs={2} sm={4} md={4}>
-          <Card
-            sx={{
-              height: '100%',
-              maxWidth: 345,
-              background: 'transparent',
-              border: '1px solid black',
-            }}
-          >
-            <CardActionArea
-              sx={{
-                height: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-              onClick={() => console.log('ADD')}
-            >
-              add board
-            </CardActionArea>
-          </Card>
-        </Grid>
+        <DialogButton
+          type="new_board"
+          btn={(handleOpen) => createAddBtn(handleOpen)}
+          form={(handleClose) => <CreateBoardForm handleClose={handleClose} />}
+        />
       </Grid>
     </>
   );
--- src/store/reducers/userSlice.ts
+++ src/store/reducers/userSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const initialState = {
-  isLogged: false,
+  isLogged: true,
   userId: '',
 };
 
