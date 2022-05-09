46c54ee65d8c2b11ae75169218903e21426cea0d
Denis Popov <popovdw94@gmail.com>
2022-05-09 18:56:22 +0300

    fix: cut board items example

--- src/components/BoardColumn/BoardColumn.tsx
+++ src/components/BoardColumn/BoardColumn.tsx
@@ -41,7 +41,7 @@ const BoardColumn = ({ order, title }: Props) => {
         className={`${styles['column']} container-scroll`}
       >
         {/* JUST AN EXAMPLE */}
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 654, 6, 546, 54, 65, 46, 546, 546, 54].map((item) => (
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
           <Box key={item} className={styles['column-item']}>
             Item #{item}
           </Box>
