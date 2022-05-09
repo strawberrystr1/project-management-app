43603a1ac317a533565c83bb4113df5799c21892
strawberrystr1 <andrey_overdose@mail.ru>
2022-05-09 19:55:42 +0300

    feat: add addaptivity for footer

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -15,15 +15,17 @@ const Footer = () => {
         <a href="https://rs.school/react/">
           <img className={styles.logo} src="./assets/rs_school_js.svg" alt="school logo" />
         </a>
-        {teamInfo.map((item) => (
-          <a href={item.github} key={item.name} className={styles.link}>
-            <img src={github} alt="github logo" className={styles.git_logo} />
-            <Typography color="white" fontSize={18}>
-              {item.name}
-            </Typography>
-          </a>
-        ))}
-        <Typography fontSize={30} fontWeight={600}>
+        <div className={styles.links_wrapper}>
+          {teamInfo.map((item) => (
+            <a href={item.github} key={item.name} className={styles.link}>
+              <img src={github} alt="github logo" className={styles.git_logo} />
+              <Typography color="white" fontSize={18}>
+                {item.name}
+              </Typography>
+            </a>
+          ))}
+        </div>
+        <Typography fontSize={30} fontWeight={600} className={styles.year}>
           2022
         </Typography>
       </Container>
