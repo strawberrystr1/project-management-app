a696307377cee158d0fa2e274e308b25a9e18bbc
strawberrystr1 <andrey_overdose@mail.ru>
2022-05-17 13:18:47 +0300

    feat: add redirect when logged

--- src/pages/authentication_page/SignUpPage.tsx
+++ src/pages/authentication_page/SignUpPage.tsx
@@ -1,9 +1,21 @@
 import { Container } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import AuthenticationForm from '../../components/AuthenticationForm';
+import { useTypedSelector } from '../../hooks/redux';
 import { signupFields } from '../../utils/constants/formFields';
 import styles from './style.module.scss';
 
 const SignUpPage = () => {
+  const { isLogged } = useTypedSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLogged) {
+      navigate('/boards');
+    }
+  }, []);
+
   return (
     <Container maxWidth="sm" className={styles.authContainer}>
       <AuthenticationForm fields={signupFields} />
