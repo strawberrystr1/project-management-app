00b16ad245bf47aebb46bcbf1c19e7d8fc8ac0de
strawberrystr1 <andrey_overdose@mail.ru>
2022-05-24 18:40:01 +0300

    refactor: change team photo and info

--- src/utils/constants/teamInfo.ts
+++ src/utils/constants/teamInfo.ts
@@ -2,14 +2,17 @@ const team = [
   {
     name: 'Andrei Shakh',
     github: 'https://github.com/strawberrystr1',
+    photo: 'andrei.jpg',
   },
   {
     name: 'Dmytro Zozuliak',
     github: 'https://github.com/DmytroZozuliak',
+    photo: 'dima.jpg',
   },
   {
     name: 'Denis Popov',
     github: 'https://github.com/denis0ff',
+    photo: 'denis.jpg',
   },
 ];
 
