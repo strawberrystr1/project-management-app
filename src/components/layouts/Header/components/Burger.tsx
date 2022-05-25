20c8624fa5fb0482da824d29acb60b774a77d16d
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-05-25 21:20:14 +0300

    feat: only on board page can see filters in burger

--- src/components/layouts/Header/components/Burger.tsx
+++ src/components/layouts/Header/components/Burger.tsx
@@ -1,15 +1,17 @@
 import { Divider, Drawer, IconButton, List, ListItem, Stack, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Box } from '@mui/system';
 import ThemeSwitcher from './ThemeSwitcher';
 import LanguageSwitch from './LanguageSwitch';
 import { useTranslation } from 'react-i18next';
 import FilterBar from '../../../BoardFilterBar/FilterBar';
+import { useMatch } from 'react-router-dom';
 
 const Burger = () => {
   const [isOpenDrawer, setIsOpenDrawer] = useState(false);
   const { t } = useTranslation();
+  const routeMatch = useMatch('/boards/:boardId');
 
   const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
     if (
@@ -26,16 +28,18 @@ const Burger = () => {
     <Box>
       <Typography sx={{ textAlign: 'center', mt: '20px' }}>{t(`header.burgerMenu`)}</Typography>
       <List>
-        <ListItem>
-          <Stack
-            sx={{ display: { xs: 'flex', md: 'none' } }}
-            minHeight={95}
-            direction="column"
-            justifyContent="flex-start"
-          >
-            <FilterBar />
-          </Stack>
-        </ListItem>
+        {routeMatch && (
+          <ListItem>
+            <Stack
+              sx={{ display: { xs: 'flex', md: 'none' } }}
+              minHeight={95}
+              direction="column"
+              justifyContent="flex-start"
+            >
+              <FilterBar />
+            </Stack>
+          </ListItem>
+        )}
         <ListItem>
           <LanguageSwitch />
         </ListItem>
