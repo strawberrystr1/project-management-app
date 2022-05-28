4bc56b7254918cfae0a06613d10fb92df42363f7
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-05-28 20:00:26 +0300

    feat: add password btn

--- src/utils/helpers/validationSchema.ts
+++ src/utils/helpers/validationSchema.ts
@@ -1,37 +1,47 @@
 import * as yup from 'yup';
+import { useTranslation } from 'react-i18next';
 
-const validationSchema = yup.object({
-  login: yup
-    .string()
-    .required('Login is required')
-    .test('only letters', 'Login should contain only letters and numbers', (value) => {
-      return !/[\&!@#$%\^\*\)\(\[\]\{\}<>,/\/\+\\]/.test(value as string);
-    })
-    .min(6, 'Login must be 6 or more characters')
-    .max(15, 'Login must be 15 characters or less'),
-  password: yup
-    .string()
-    .trim()
-    .test('password', `Password mustn't contain the whitespaces`, (value) => {
-      return !/\s/.test(value as string);
-    })
-    .required('Password is required')
-    .test(
-      'password',
-      `Password mustn't contain the following characters '@, #, $, %, ^, &, *'`,
-      (value) => {
-        return !/[\&@#$%\^\*]/.test(value as string);
-      }
-    )
-    .min(8, 'Password must be 8 or more characters'),
-  name: yup
-    .string()
-    .required('Name is required')
-    .test('only letters', 'Name should contain only letters and numbers', (value) => {
-      return !/[\&!@#$%\^\*\)\(\[\]\{\}<>,/\/\+\\]/.test(value as string);
-    })
-    .min(3, 'Name must be 3 or more characters')
-    .max(20, 'Name must be 20 or less characters'),
-});
+const useValidationSchema = () => {
+  const { t } = useTranslation();
 
-export default validationSchema;
+  // {t('forms.auth.')}
+  // t('forms.auth.')
+
+  const validationSchema = yup.object({
+    login: yup
+      .string()
+      .required(t('forms.auth.login_required'))
+      .test('only letters', 'Login should contain only letters and numbers', (value) => {
+        return !/[\&!@#$%\^\*\)\(\[\]\{\}<>,/\/\+\\]/.test(value as string);
+      })
+      .min(6, 'Login must be 6 or more characters')
+      .max(15, 'Login must be 15 characters or less'),
+    password: yup
+      .string()
+      .trim()
+      .test('password', `Password mustn't contain the whitespaces`, (value) => {
+        return !/\s/.test(value as string);
+      })
+      .required('Password is required')
+      .test(
+        'password',
+        `Password mustn't contain the following characters '@, #, $, %, ^, &, *'`,
+        (value) => {
+          return !/[\&@#$%\^\*]/.test(value as string);
+        }
+      )
+      .min(8, 'Password must be 8 or more characters'),
+    name: yup
+      .string()
+      .required('Name is required')
+      .test('only letters', 'Name should contain only letters and numbers', (value) => {
+        return !/[\&!@#$%\^\*\)\(\[\]\{\}<>,/\/\+\\]/.test(value as string);
+      })
+      .min(3, 'Name must be 3 or more characters')
+      .max(20, 'Name must be 20 or less characters'),
+  });
+
+  return validationSchema;
+};
+
+export default useValidationSchema;
