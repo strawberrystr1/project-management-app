5de14bee41a1151736b1a48c37337be2c50892a2
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2022-05-28 21:11:10 +0300

    feat: add translation from hooks to errors

--- src/utils/helpers/validationSchema.ts
+++ src/utils/helpers/validationSchema.ts
@@ -4,41 +4,34 @@ import { useTranslation } from 'react-i18next';
 const useValidationSchema = () => {
   const { t } = useTranslation();
 
-  // {t('forms.auth.')}
-  // t('forms.auth.')
-
   const validationSchema = yup.object({
     login: yup
       .string()
       .required(t('forms.auth.login_required'))
-      .test('only letters', 'Login should contain only letters and numbers', (value) => {
+      .test('only letters', t('forms.auth.login_letters'), (value) => {
         return !/[\&!@#$%\^\*\)\(\[\]\{\}<>,/\/\+\\]/.test(value as string);
       })
-      .min(6, 'Login must be 6 or more characters')
-      .max(15, 'Login must be 15 characters or less'),
+      .min(6, t('forms.auth.login_min'))
+      .max(15, t('forms.auth.login_max')),
     password: yup
       .string()
       .trim()
-      .test('password', `Password mustn't contain the whitespaces`, (value) => {
+      .test('password', t('forms.auth.password_whitespaces'), (value) => {
         return !/\s/.test(value as string);
       })
-      .required('Password is required')
-      .test(
-        'password',
-        `Password mustn't contain the following characters '@, #, $, %, ^, &, *'`,
-        (value) => {
-          return !/[\&@#$%\^\*]/.test(value as string);
-        }
-      )
-      .min(8, 'Password must be 8 or more characters'),
+      .required(t('forms.auth.password_required'))
+      .test('password', t('forms.auth.password_characters'), (value) => {
+        return !/[\&@#$%\^\*]/.test(value as string);
+      })
+      .min(8, t('forms.auth.password_min')),
     name: yup
       .string()
-      .required('Name is required')
-      .test('only letters', 'Name should contain only letters and numbers', (value) => {
+      .required(t('forms.auth.name_required'))
+      .test('only letters', t('forms.auth.name_letters'), (value) => {
         return !/[\&!@#$%\^\*\)\(\[\]\{\}<>,/\/\+\\]/.test(value as string);
       })
-      .min(3, 'Name must be 3 or more characters')
-      .max(20, 'Name must be 20 or less characters'),
+      .min(3, t('forms.auth.name_min'))
+      .max(20, t('forms.auth.name_max')),
   });
 
   return validationSchema;
